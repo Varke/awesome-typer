2163131a7792f2a0940dc4abb437c13bee2f73e0
Varke <mr.varke@mail.ru>
2023-08-26 17:11:20 +0300

    main part

--- src/components/TyperScreen.js
+++ src/components/TyperScreen.js
@@ -1,24 +1,35 @@
 import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 const TyperScreen = () => {
-   const str =
-      'Nam molestie, eros id mattis hendrerit, tortor nisl auctor nunc, eu vulputate magna nisl id torto';
-   const strArray = str.split('');
-   const [newArray, setNewArray] = useState([]);
+   const originalString =
+      'No one shall be subjected to arbitrary arrest, detention or exile. Everyone is entitled in full equality to a fair and public hearing by an independent and impartial tribunal, in the determination of his rights and obligations and of any criminal charge against him. No one shall be subjected to arbitrary interference with his privacy, family';
+   const [inputString, setInputString] = useState('');
 
-   const isCharacterActive = (index) => {
-      console.log(newArray[index], ' == ', strArray[index]);
-      if (newArray.length > index && newArray[index] == strArray[index])
-         return true;
-      return false;
+   const getColorForChar = (char, index) => {
+      if (index >= inputString.length) {
+         return '#939eae'; // Серый цвет для неактивных символов
+      }
+      if (inputString[index] === char) {
+         return 'white'; // Белый цвет для совпавших символов
+      }
+      return '#ca4754'; // Красный цвет для несовпавших символов
    };
 
+   // Обработчик нажатия клавиши
    useEffect(() => {
       const handleKeyDown = (event) => {
-         if (event.key != 'Shift') {
-            setNewArray((prevArray) => [...prevArray, event.key]);
-            console.log(newArray);
+         // Исключаем Ctrl, Alt, и Meta (Cmd на Mac)
+         if (!event.ctrlKey && !event.altKey && !event.metaKey) {
+            // Если нажали Backspace, нужно удалить последний введённый символ
+            if (event.key === 'Backspace') {
+               setInputString(inputString.slice(0, -1));
+            }
+            // Обрабатываем только символы длиной 1, чтобы не трогать служебные
+            else if (event.key.length === 1) {
+               setInputString(inputString + event.key);
+               console.log(inputString);
+            }
          }
       };
 
@@ -27,24 +38,64 @@ const TyperScreen = () => {
       return () => {
          window.removeEventListener('keydown', handleKeyDown);
       };
-   }, [newArray]);
+   }, [inputString]);
 
    return (
       <Container>
          <TextContainer>
-            {strArray.map((character, index) => (
-               <Text key={index} active={isCharacterActive(index)}>
-                  {character}
-               </Text>
-            ))}
+            {/* <Text ref={componentRef}>{str}</Text> */}
+            <TextWithLetters>
+               {originalString.split('').map((char, index) => (
+                  <Letter
+                     color={getColorForChar(char, index)}
+                     needShowBeforeBlock={index === inputString.length}
+                  >
+                     {char}
+                  </Letter>
+               ))}
+            </TextWithLetters>
          </TextContainer>
       </Container>
    );
 };
 
+const blinkingAnimation = keyframes`
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+  }
+`;
+
+const Letter = styled.span`
+   position: relative;
+   font-size: 24px;
+   font-family: 'Noto Sans Mono', monospace;
+   font-weight: 400;
+   user-select: none;
+   color: ${(props) => props.color};
+   &::before {
+      content: '';
+      display: ${(props) =>
+         props.needShowBeforeBlock
+            ? 'block'
+            : 'none'}; // Блоковое отображение для ::before
+      position: absolute;
+      left: 0;
+      top: 0;
+      height: 100%;
+      width: 2px;
+      background-color: green;
+      animation: ${blinkingAnimation} 0.6s infinite alternate;
+   }
+`;
+
 const TextContainer = styled.div`
    display: flex;
-   white-space: pre-wrap;
+   flex-direction: column;
+   justify-content: flex-start;
+   width: 80%;
 `;
 
 const Container = styled.div`
@@ -55,18 +106,18 @@ const Container = styled.div`
    flex-direction: column;
    justify-content: center;
    align-items: center;
-   padding-inline: 100px;
 `;
 
-const Text = styled.p`
+const Text = styled.span`
    color: ${(props) => (props.active ? '#FFF' : '#939eae')};
-   font-size: 36px;
-   font-family: Bricolage Grotesque;
-   font-weight: 500;
+   font-size: 24px;
+   font-family: 'Noto Sans Mono', monospace;
+   font-weight: 400;
    user-select: none;
+   margin-top: 0;
 `;
 
-const UpText = styled(Text)`
+const TextWithLetters = styled(Text)`
    color: white;
 `;
 
